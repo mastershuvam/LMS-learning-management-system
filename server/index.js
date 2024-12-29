27c6af15aa6cb7fbdc05a32f98b6a9eb060f09da
Shuvam Ghosh <shuvamghosh375@gmail.com>
2024-12-29 16:09:39 +0530

    Add mongoose dependency and implement MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectDB from "./database/db.js";
 
 
 dotenv.config({});
 
 // call database connection here
+connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
