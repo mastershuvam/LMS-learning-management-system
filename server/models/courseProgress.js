2556b7d28cd8da515e38aff45a84385a3e85a4b2
Shuvam Ghosh <shuvamghosh375@gmail.com>
2024-12-30 18:32:27 +0530

    Add course progress, purchase, and lecture models; improve MongoDB connection error logging

---
+++ server/models/courseProgress.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const lectureProgressSchema = new mongoose.Schema({
+    lectureId:{type:String},
+    viewed:{type:Boolean}
+});
+
+const courseProgressSchema = new mongoose.Schema({ 
+    userId:{
+        type: String,
+    },
+    courseId:{
+        type: String,
+    },
+    LectureProgress: [
+        lectureProgressSchema
+    ],
+ },);
+
+ export const CourseProgress = mongoose.model("CourseProgress", courseProgressSchema);  
